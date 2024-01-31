d7c41063f1f6c310827d28c465aacddd18ed5d9e
Michael Pollack <michaelppollack@gmail.com>
2024-01-31 13:46:14 -0800

    new grammar update

--- tsdb/skeletons/Index.lisp
+++ tsdb/skeletons/Index.lisp
@@ -21,5 +21,5 @@
 
 (
 ((:path . "matrix") (:content . "matrix: A test suite created automatically from the test sentences given in the Grammar Matrix questionnaire."))
-((:path . "lab3") (:content . "Test suite collected for Lab 3."))
+((:path . "lab4") (:content . "Test suite collected for Lab 4."))
 )
