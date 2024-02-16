6cbec79bae6f5554944b49d94cdb4920f47d799b
Michael Pollack <michaelppollack@gmail.com>
2024-02-16 11:04:55 -0800

    skeleton updates

--- tsdb/skeletons/Index.lisp
+++ tsdb/skeletons/Index.lisp
@@ -23,4 +23,5 @@
 ((:path . "matrix") (:content . "matrix: A test suite created automatically from the test sentences given in the Grammar Matrix questionnaire."))
 ; New test suites here. For example:
 ((:path . "lab5") (:content . "Lab 5 test suite."))
+((:path . "lab6") (:content . "Lab 6 test suite."))
 )
